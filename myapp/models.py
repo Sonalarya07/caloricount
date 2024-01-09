1e6413d7a53f418b675982d0a56664bf6a684b99
Sonalarya07 <150377067+Sonalarya07@users.noreply.github.com>
2024-01-09 19:23:53 +0530

    initil commit

--- myapp/models.py
+++ myapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -12,4 +13,9 @@ class Food(models.Model):
     protein = models.FloatField()
     fat = models.FloatField()
     calories = models.IntegerField()
+
+class Consume(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    food_consumed = models.ForeignKey(Food,on_delete=models.CASCADE)
+    
     
